f27613d1865b04412ae5d51a99cd59e726c8da45
Jan-Oliver Jahner <jan-oliver.jahner@xing.com>
2014-09-18 13:55:00 +0200

    use lein 2.x.x

--- project.clj
+++ project.clj
@@ -10,6 +10,7 @@
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler short.handler/app}
   :main short.handler
+  :min-lein-version "2.0.0"
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring-mock "0.1.5"]]}})
